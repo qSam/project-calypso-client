89d40a4ae98d6c505238deb7ecf83ee77b57ae2c
Qasim Laghari <go.jets.laghari@gmail.com>
2016-12-31 15:39:18 -0600

    Submit setup for react router

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -3,5 +3,52 @@ import {reduxForm} from 'redux-form';
 import * as actions from '../../actions';
 
 class Signup extends Component {
-  
+
+  handleFormSubmit(formProps){
+    //Call Signup Action Creator
+  }
+
+  render() {
+    const {handleSubmit, fields:{user, password, passwordConfirm}} = this.props;
+
+    return(
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+
+      <fieldset className="form-group">
+        <label>User Name:</label>
+        <input className="form-control" {...user} />
+      </fieldset>
+
+      <fieldset className="form-group">
+        <label>Password:</label>
+        <input className="form-control" {...password} type="password" />
+      </fieldset>
+
+      <fieldset className="form-group">
+        <label>Confirm Password:</label>
+        <input className="form-control" {...passwordConfirm} type="password" />
+      </fieldset>
+
+
+        <button action="submit" className="btn btn-primary">Sign Up!</button>
+      </form>
+
+    );
+  }
 }
+
+function validate(formProps){
+  const errors = {};
+
+  return errors;
+}
+
+function mapStateToProps(state) {
+  return {};
+}
+
+export default reduxForm({
+  form: 'signup',
+  fields: ['user', 'password', 'passwordConfirm' ],
+  validate:validate
+}, mapStateToProps, actions)(Signup);
--- src/index.js
+++ src/index.js
@@ -2,8 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
+import {Provider} from 'react-redux';
+
+//Import Components
+import Signup from './components/auth/signup';
+
+//Create Redux Store
+const store = {};
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Route path="/" component={App}>
+      <IndexRoute component={App} />
+      <Route path='signup' component={Signup} />
+    </Route>
+  </Provider>,
   document.getElementById('root')
 );
