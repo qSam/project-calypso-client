d9f1f376757dd033881ba2224cb14ba61e6e846d
Qasim Laghari <go.jets.laghari@gmail.com>
2017-01-03 21:29:41 -0600

    Setup Redux for App

--- src/index.js
+++ src/index.js
@@ -4,20 +4,25 @@ import App from './App';
 import './index.css';
 import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
 
 //Import Components
+import reducers from './reducers';
 import Signup from './components/auth/signup';
+import Signin from './components/auth/signin';
 import Welcome from './components/welcome';
 
 //Create Redux Store
-const store = {};
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 ReactDOM.render(
   <Provider store={store}>
   <Router history={browserHistory}>
     <Route path="/" component={App}>
-      <IndexRoute component={Signup} />
+      <IndexRoute component={Welcome} />
       <Route path='signup' component={Signup} />
+      <Route path='signin' component={Signin} />
     </Route>
   </Router>
   </Provider>,
