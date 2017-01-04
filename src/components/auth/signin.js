4a1ca2644eaf9c8e9f363ee560edd1973ac5b9a4
Qasim Laghari <go.jets.laghari@gmail.com>
2017-01-03 22:34:10 -0600

    Submit signin form

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,3 +1,37 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {reduxForm} from 'redux-form';
+import * as actions from '../../actions';
 
-export default () => <div> Welcome to the Signin Route</div>
+class Signin extends Component {
+
+  handleFormSubmit(formProps) {
+    //Call Signin Action Creator
+  }
+
+  render() {
+    const {handleSubmit, fields:{user, password}} = this.props;
+
+    return (
+      <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+
+      <button action="submit" className="btn btn-primary">Sign In!</button>
+    </form>
+  );
+  }
+}
+
+function validate(formProps){
+  const errors = {};
+
+  return errors;
+}
+
+function mapStateToProps(state){
+  return {};
+}
+
+export default reduxForm({
+  form: 'signin',
+  fields: ['user', 'password'],
+  validate: validate
+}, mapStateToProps, actions)(Signin);
