d22c8087713314fb262f5dfe427ff3965e9c33f0
Qasim Laghari <go.jets.laghari@gmail.com>
2017-01-06 17:11:18 -0600

    Update contact component

--- src/components/newPool/memberContacts.js
+++ src/components/newPool/memberContacts.js
@@ -1,15 +1,54 @@
 import React, {Component} from 'react';
+import * as actions from '../../actions';
+import {connect} from 'react-redux';
+import {browserHistory} from 'react-router';
+import _ from 'lodash';
 
 
 class MemberContacts extends Component {
 
+  constructor(props) {
+    super(props);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event){
+    console.log(event.target.input_0.value);
+    event.preventDefault();
+  }
+
+  renderInputs(){
+      return(
+        <div>
+        { _.times(this.props.totalMembers, (i) => {
+           let keyString="";
+           keyString="input_".concat(i);
+           console.log(keyString);
+           return (
+             <div className="input-group" key={keyString} >
+             <input type="text" className="form-control" name={keyString} placeholder="Email" />
+             </div>
+        );
+      })}
+        </div>
+      );
+  }
+
   render(){
     return (
       <div>
-        Please enter contacts for pool members
+        <form onSubmit={this.handleSubmit}>
+        Please enter contacts for {this.props.totalMembers} pool members
+        {this.renderInputs()}
+        <input className="btn btn-primary" type="submit" value="Next" />
+        </form>
       </div>
     );
   }
 }
 
-export default MemberContacts;
+function mapStateToProps(state) {
+  return {totalMembers: state.pool.totalMembers};
+}
+
+export default MemberContacts = connect(mapStateToProps, actions)(MemberContacts);
