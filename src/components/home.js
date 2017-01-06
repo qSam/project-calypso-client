d79c13599427c805f4cd23760a9380c8353397b7
Qasim Laghari <go.jets.laghari@gmail.com>
2017-01-06 11:16:13 -0600

    Update home feature

--- src/components/home.js
+++ src/components/home.js
@@ -1,3 +1,29 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-export default () => <div>Home Page for Project Calypso</div>
+
+class Home extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleSubmit(event) {
+
+    event.preventDefault();
+  }
+
+  render(){
+    return (
+      <div className="text-left">
+        This is the Home Component
+        <form onSubmit={this.handleSubmit}>
+          <input className="btn btn-primary" type="submit" value="Create new savings pool" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Home;
