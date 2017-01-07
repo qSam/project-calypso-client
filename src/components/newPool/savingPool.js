86386785eccbd359a1a13b184ff845505f57f3e8
Qasim Laghari <go.jets.laghari@gmail.com>
2017-01-07 16:44:46 -0600

    Update saving pool component with Redux Form

--- src/components/newPool/savingPool.js
+++ src/components/newPool/savingPool.js
@@ -1,15 +1,54 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
+import * as actions from '../../actions';
+import {connect} from 'react-redux';
 
 
 class SavingPool extends Component {
 
+  handleFormSubmit(values) {
+    //Call an action creator here
+  }
+
   render() {
+    const {handleSubmit} = this.props;
+
     return (
-      <div>
+      <div className="text-left">
+        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))} >
         Create a saving pool
+
+        <fieldset className="form-group">
+          <label htmlFor="policyName">Policy Name</label>
+          <Field name="policyName" className="form-control" component="input" type="text" />
+        </fieldset>
+
+        <fieldset className="form-group">
+          <label htmlFor="totalAmount">Total Amount $</label>
+          <Field name="totalAmount" className="form-control" component="input" type="text" />
+        </fieldset>
+
+        <button action="submit" className="btn btn-primary">Next</button>
+
+        </form>
       </div>
     )
   }
 }
 
-export default SavingPool;
+function validate(formProps) {
+  const errors = {};
+
+  return errors;
+}
+
+function mapStateToProps(state){
+  return {};
+}
+
+SavingPool = reduxForm({
+  form:'savingpool',
+  validate:validate
+})(SavingPool);
+
+export default SavingPool = connect(mapStateToProps, actions)(SavingPool);
