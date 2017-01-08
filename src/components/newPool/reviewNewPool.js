6f584a7daa56b7a6fe34e50473fe21579fcb9cac
Qasim Laghari <go.jets.laghari@gmail.com>
2017-01-07 20:58:22 -0600

    Update review component

--- src/components/newPool/reviewNewPool.js
+++ src/components/newPool/reviewNewPool.js
@@ -1,17 +1,62 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
 
 class ReviewNewPool extends Component {
 
+
+  renderTotalMembers() {
+    return (
+      <div>
+        Total number of members : {this.props.totalMembers}
+      </div>
+    );
+  }
+
+  renderMemberContacts(){
+    return(
+      <div>
+        {this.props.memberContacts.map( (contact) => {
+          return (
+            <div key={contact.Email}>
+                Member contact is {contact.Email}
+            </div>
+          );
+        }
+        )}
+      </div>
+    );
+  }
+
+  renderPoolDetails() {
+    return(
+      <div>
+        Pool Name : {this.props.poolDetails.policyName} <br />
+        Total Amount : {this.props.poolDetails.totalAmount} <br />
+        Pool Length : {this.props.poolDetails.policyLength} month(s)
+      </div>
+    );
+  }
+
   render(){
 
     return(
-      <div>
-        Review New Pool
+      <div className="panel panel-default text-left">
+        {this.renderPoolDetails()}
+        {this.renderTotalMembers()}
+        {this.renderMemberContacts()}
       </div>
     );
   }
 }
 
+function mapStateToProps(state) {
+  return {
+    totalMembers: state.pool.totalMembers,
+    memberContacts: state.pool.memberContacts,
+    poolDetails: state.pool.newPoolDetails
+  };
+}
 
-export default ReviewNewPool;
+
+export default ReviewNewPool = connect (mapStateToProps, null)(ReviewNewPool);
